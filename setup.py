e5df1fb6c7c205149e00c5d47f6e1fdefd7bca67
Marco S. Nobile <nobile@disco.unimib.it>
2018-09-26 15:00:33 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+setup(
+  name = 'simpful',
+  packages = ['simpful'], # this must be the same as the name above
+  version = '1.0.0',
+  description = 'A simple Fuzzy Logic library',
+  author = 'Marco S. Nobile',
+  author_email = 'nobile@disco.unimib.it',
+  url = 'https://github.com/aresio/simpful', # use the URL to the github repo
+  keywords = ['fuzzy logic', 'sugeno', 'reasoner', 'python'], # arbitrary keywords
+  license='LICENSE.txt',
+  install_requires=[
+        "numpy >= 1.12.0",
+        "scipy >= 1.0.0"
+    ],
+  classifiers = ['Programming Language :: Python :: 2.7'],
+)
---
+++ simpful/__init__.py
@@ -0,0 +1,1 @@
+from simpful import *
