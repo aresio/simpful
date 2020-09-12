e186019c935c45ee38ca01fc68bc2b0301321788
Nikhilrs1993 <33655053+Nikhilrs1993@users.noreply.github.com>
2020-09-12 12:17:27 +0200

    removes pylab version due to compatibility issues

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
   install_requires=[
         "numpy >= 1.12.0",
         "scipy >= 1.0.0",
-        "pylab >= 1.3.2"
+        "pylab"
     ],
   classifiers = ['Programming Language :: Python :: 3.7'],
   long_description='simpful is a Python library for fuzzy logic reasoning, \
