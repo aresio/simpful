007c6b3a4ae715a5f48dc5f3d350d0fc1f292fd2
Nikhilrs1993 <33655053+Nikhilrs1993@users.noreply.github.com>
2020-10-08 04:41:17 +0200

    Updated Testing of PostParse; Changed 'is' to 'IS'

--- tests/test_rule_parsing.py
+++ tests/test_rule_parsing.py
@@ -9,9 +9,7 @@ def test_postparse():
     output = rule_parsing.postparse(normal_rule)
     assert output == expected_out_normal
 
-    proba_rule = "IF (OXI IS low_flow) THEN P(POWER is LOW_POWER)=0.33, P(POWER is MEDIUM_POWER)=0.33, P(POWER is HIGH_FUN)=0.34"
-    expected_out_proba = ('POWER', 'LOW_POWER', '0.33', 'POWER', 'MEDIUM_POWER', '0.33', 'POWER', 'HIGH_FUN', '0.34')
-    output = rule_parsing.postparse(proba_rule)
-    print(output)
-    print(expected_out_proba)
-    assert output == expected_out_proba
+    proba_rule = "IF (OXI IS low_flow) THEN P(POWER IS LOW_POWER)=0.33, P(POWER IS MEDIUM_POWER)=0.33, P(POWER IS HIGH_FUN)=0.34"
+    expected_out_proba = ([0.33, 0.33, 0.34], ['POWER', 'LOW_POWER', '0.33', 'POWER', 'MEDIUM_POWER', '0.33', 'POWER', 'HIGH_FUN', '0.34'])
+    output_proba = rule_parsing.postparse(proba_rule)
+    assert output_proba == expected_out_proba
