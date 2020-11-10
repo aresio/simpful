e2d067961fe30d9c858c6bfba3d4d16ff8044053
Simone Spolaor <s.spolaor@campus.unimib.it>
2020-11-10 18:34:52 +0100

    minors setup

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
-from distutils.core import setup
+from setuptools import setup
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
   name = 'simpful',
   packages = ['simpful'], # this must be the same as the name above
@@ -15,5 +21,6 @@ setup(
         "requests",
     ],
   classifiers = ['Programming Language :: Python :: 3.7'],
-  long_description = open('README.md').read(),
+  long_description=long_description,
+  long_description_content_type='text/markdown',
 )
