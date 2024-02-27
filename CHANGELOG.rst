796afc6627d2b17cb3e1a1bcfc0dbf1b9de6bd01
Marco S. Nobile <marco.nobile@unive.it>
2024-02-27 12:09:57 +0100

    New license

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,3 +1,9 @@
+2.12.0 (2024-02-13)
+------------------
+* Introduced support for categorical variables in consequent evaluation with template replacement. 
+* Changed license to AFL v3.0
+
+
 2.8.0 (2022-09-07)
 ------------------
 * Introduced support for arbitrary aggregation functions in Mamdani reasoning.
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
   name = 'simpful',
   packages = ['simpful'], # this must be the same as the name above
-  version = '2.11.1',
+  version = '2.12.0',
   description = 'A user-friendly Python library for fuzzy logic',
   author = 'Marco S. Nobile',
   author_email = 'marco.nobile@unive.it',
@@ -24,7 +24,7 @@ setup(
     },
   classifiers = [
     "Programming Language :: Python :: 3",
-    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "License :: OSI Approved :: Academic Free License (AFL)",
     "Operating System :: OS Independent",],
   long_description=long_description,
   long_description_content_type='text/markdown',
