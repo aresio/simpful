c96930f6b0d7e442ac37f5ee84d13a761d294998
Nikhil Razab-Sekh <nikhil_rs@live.com>
2024-05-28 18:59:55 +0200

    chore: cleaning. TODO: make progress bar visible

--- simpful/gp_fuzzy_system/tests/test_run_gp_simulation.py
+++ simpful/gp_fuzzy_system/tests/test_run_gp_simulation.py
@@ -11,26 +11,6 @@ class TestRunGPSimulation(unittest.TestCase):
         cls.y_train_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'gp_data_y_train.csv'))
         cls.terms_dict_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'terms_dict.py'))
 
-    def test_run_with_verbose(self):
-        result = subprocess.run([
-            "python", self.script_path,
-            "--x_train", self.x_train_path,
-            "--y_train", self.y_train_path,
-            "--terms_dict_path", self.terms_dict_path,
-            "--exclude_columns", "month,day,hour",
-            "--population_size", "30",
-            "--max_generations", "5",
-            "--mutation_rate", "0.01",
-            "--selection_method", "tournament",
-            "--crossover_rate", "0.8",
-            "--elitism_rate", "0.1",
-            "--max_rules", "7",
-            "--min_rules", "3",
-            "--verbose"
-        ], capture_output=True, text=True)
-        
-        self.assertEqual(result.returncode, 0, f"Script failed with return code {result.returncode}. Output: {result.stdout}\nError: {result.stderr}")
-
     def test_run_without_verbose(self):
         result = subprocess.run([
             "python", self.script_path,
