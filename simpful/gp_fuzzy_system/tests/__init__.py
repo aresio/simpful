4c2cca993e972e07339f0a0ba6eafa2a17155fa0
Nikhil Razab-Sekh <nikhil_rs@live.com>
2024-05-28 23:36:34 +0200

    package tests

---
+++ simpful/gp_fuzzy_system/tests/__init__.py
@@ -0,0 +1,12 @@
+# This __init__.py file will make the tests folder a package
+# You can import the necessary modules here if needed
+
+# For example:
+from .instances import *
+from .test_evolvable_fuzzy_system import *
+from .test_fitness_evaluation import *
+from .test_gp_evolution import *
+from .test_gp_utilities import *
+from .test_rule_generator import *
+from .test_rule_processor import *
+from .test_run_gp_simulation import *
